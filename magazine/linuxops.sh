8f39b01d5516daf3cbfbf7a2ba32ddf0ef2f953a
cfddream <cfddream@gmail.com>
2011-05-21 01:10:00 +0800

    download 《Linux运维趋势》

---
+++ magazine/linuxops.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+NUM=${*:-0}
+SITE="http://os.51cto.com/os/linuxops/pdf"
+FILE_TEMP="51CTO_linuxops_issue%d.pdf"
+Documents=~/Documents
+echo $NUM
+
+for p in $NUM;
+do
+    FILE_NAME=`printf $FILE_TEMP $p`
+    if [ ! -f "$Documents/$FILE_NAME" ]; then
+        ISSUE="$SITE/$FILE_NAME"
+        wget -c -O "$Documents/$FILE_NAME" $ISSUE
+        echo "Download $FILE_NAME successy!"
+    fi
+done;
