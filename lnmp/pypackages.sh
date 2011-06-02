f62cf11a8e180fda990e1297b2c1ffd882b6c87a
cfddream <cfddream@gmail.com>
2011-06-02 15:03:20 +0800

    create python_version env dir

--- lnmp/pypackages.sh
+++ lnmp/pypackages.sh
@@ -7,12 +7,13 @@
 # > deactivate
 
 PYVENVS="$HOME/develop/lnmp/pyvenvs"
-ENV="env2.7"
+ENV="env`python -V 2>&1 | awk '{print substr($2, 1, 3)}'`"
 
 if [ ! -d "$PYVENVS/$ENV" ]; then
     mkdir -p $PYVENVS/$ENV
 fi
 
+virtualenv $PYVENVS/$ENV
 . $PYVENVS/$ENV/bin/activate
 pip install --upgrade redis
 pip install --upgrade MySQL-python
