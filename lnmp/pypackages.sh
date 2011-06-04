e1b3a03940f1ad175e85e6424c7b3ac53750d960
cfddream <cfddream@gmail.com>
2011-06-05 02:38:45 +0800

    install phpsh

--- lnmp/pypackages.sh
+++ lnmp/pypackages.sh
@@ -15,9 +15,24 @@ fi
 
 virtualenv $PYVENVS/$ENV
 . $PYVENVS/$ENV/bin/activate
+
 pip install --upgrade redis
 pip install --upgrade MySQL-python
+
+# web frameworks
 pip install --upgrade Flask
+pip install --upgrade django
+pip install --upgrade pylons
+pip install --upgrade tornado
+pip install --upgrade web.py
+pip install --upgrade bottle
+
+# wsgi
 pip install --upgrade gunicorn
 pip install --upgrade flup
 pip install --upgrade uwsgi
+
+# phpsh
+#git clone git://github.com/facebook/phpsh.git
+#cd phpsh
+#python setup.py install
