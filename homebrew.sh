76bcd26ca6f8cfb37d241e4622d8907e55e2e23d
cfddream <cfddream@gmail.com>
2012-03-23 00:05:45 +0800

    update

--- homebrew.sh
+++ homebrew.sh
@@ -4,34 +4,45 @@ mkdir $HOMEBREW && curl -L https://github.com/mxcl/homebrew/tarball/master | tar
 brew update
 brew upgrade
 brew install zsh
+brew install bash-completion
+brew install ssh-copy-id
+brew install grc
 brew install git
+brew install tig
+brew install git-flow
+brew install git-extras
 brew install xz
-brew install --HEAD https://raw.github.com/adamv/homebrew/master/Library/Formula/coreutils.rb
+brew install coreutils
 brew install gettext
 brew link gettext
 brew install libevent
 brew install tmux
+brew install ruby
 brew install wget
-brew install curl 
+brew install curl -f
+brew link curl
 brew install unrar
 brew install aria2
 brew install ack
 brew install lua
 brew install ctags
 brew install tree
+brew install autojump
 #brew install macvim --override-system-vim
 brew install macvim 
 brew install mercurial
-brew install --HEAD https://raw.github.com/adamv/homebrew-alt/master/duplicates/vim.rb
+brew install https://raw.github.com/adamv/homebrew-alt/master/duplicates/vim.rb
 brew install --HEAD htop
 brew install spark
 brew install pwgen
 ln -sf ~/develop/homebrew/Cellar/macvim/7.3-63/MacVim.app ~/DevApps
 
 brew install --HEAD https://raw.github.com/adamv/homebrew-alt/master/duplicates/php.rb
-brew install mysql
+#brew install mysql
+brew install https://raw.github.com/gist/1187345/3daa0f4d9602dd20c07ea5c4be763d3dcceb0ab6/percona-server.rb
+brew link percona-server
 unset TMPDIR
-mysql_install_db --verbose --user=`whoami` --basedir="$(brew --prefix mysql)" --datadir=~/develop/data/mysql --tmpdir=/tmp
+mysql_install_db --verbose --user=`whoami` --basedir="$(brew --prefix percona-server)" --datadir="$HOME/develop/data/percona" --tmpdir=/tmp
 # /etc/my.cnf ~/.my.cnf
 mysql.server start
 mysql_secure_installation
@@ -46,4 +57,5 @@ brew install webbench
 brew install cmatrix
 brew install mongodb
 brew install memcached
+brew install autossh
 brew cleanup
