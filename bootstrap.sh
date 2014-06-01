e8f6621bce042c03cf829554efb490a0752b2916
fundon <cfddream@gmail.com>
2014-06-01 22:03:35 +0800

    add bootstrap sh

---
+++ bootstrap.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env zsh
+git pull origin master
+function doIt() {
+  rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
+    --exclude "README.md" --exclude "LICENSE-MIT.txt" \
+    --exclude ".zsh/" --exclude ".zshrc" --exclude "pip.conf" \
+    -avh --no-perms . ~
+  source ~/.zshrc
+}
+if [[ "$1" == "--force" ]] || [[ "$1" == "-f" ]]; then
+  doIt
+else
+  echo -n  "This may overwrite existing files in your home directory. Are you sure? (y/n) "
+  read REPLY
+  echo
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    doIt
+  fi
+fi
+unset doIt
