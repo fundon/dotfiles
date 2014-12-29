93c83e27bfbb1ec3c8bf5cf15012df66741770b7
fundon <cfddream@gmail.com>
2014-12-29 14:01:10 +0800

    update

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,7 @@ function doIt() {
   rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
     --exclude "README.md" --exclude "LICENSE-MIT.txt" \
     --exclude "Brewfile" \
+    --exclude ".zshrc.local" \
     -avh --no-perms . ~
   source ~/.zshrc
 }
